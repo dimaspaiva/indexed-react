5f982e08efb80dca1c338793b3f5fb93c4731ac3
dimaspaiva <dimasalpaiva@gmail.com>
2020-03-31 17:12:07 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const request = window.indexedDB.open('training', 1);
+
+  request.onerror = () => {};
+
+  request.onupgradeneeded = () => {
+    const { result: db } = request;
+    console.log(db);
+    db.createObjectStore('shopCartStore', { keyPath: 'id' });
+  };
+
+  request.onsuccess = () => {
+    const { result: db } = request;
+    const transaction = db.transaction(['shopCartStore'], 'readwrite');
+    const objectStore = transaction.objectStore('shopCartStore');
+
+    const insert = objectStore.put({
+      id: 1,
+      name: 'dimas',
+      lastName: 'paiva',
+      age: 22
+    });
+
+    insert.onsuccess = () => {
+      console.log('worked!');
+    };
+
+    insert.onerror = (e) => {
+      console.log('failed');
+      console.log(e);
+    };
+
+    const get = objectStore.get(1);
+
+    get.onsuccess = (e) => {
+      console.log(e.target.result);
+    };
+  };
+
+  return <div className="App">Learn React</div>;
 }
-
-export default App;
